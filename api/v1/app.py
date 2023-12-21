41462c17abd8964d42623b2bb93ce2851996e98e
Eonvorax <mail4bishop@gmail.com>
2023-12-21 10:42:13 +0100

    Task 6: Not found (404)

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 This is the app module
 """
 
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -18,6 +18,15 @@ def teardown_db(exception):
     Closes the storage on teardown
     """
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    """
+    Handle 404 errors and return a JSON response.
+    """
+    return jsonify({"error": "Not found"}), 404
+
 
 if __name__ == "__main__":
     api_host = getenv("HBNB_API_HOST", "0.0.0.0")
