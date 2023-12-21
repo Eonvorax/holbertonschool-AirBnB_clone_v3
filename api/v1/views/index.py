e310e64d2cd7bad7d8a0a1710e238e62eec3112c
Eonvorax <mail4bishop@gmail.com>
2023-12-21 10:07:28 +0100

    Task 5: Get stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,6 +6,7 @@ Routes module for api
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', strict_slashes=False)
@@ -14,3 +15,20 @@ def status():
     Returns the status of the server in JSON format
     """
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/api/v1/stats", strict_slashes=False)
+def get_stats():
+    """
+    Retrieves the count of each class instance by class
+    """
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+
+    return jsonify(stats)
